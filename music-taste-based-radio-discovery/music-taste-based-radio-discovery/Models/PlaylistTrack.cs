e3ca59b495f472992b539931bbd570703e411b36
Mattias Rudén <mattias@indiesoft.org>
2016-11-17 15:09:19 +0100

    Repository för att hitta spelade låtar i isidordatabasen

---
+++ music-taste-based-radio-discovery/music-taste-based-radio-discovery/Models/PlaylistTrack.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace music_taste_based_radio_discovery.Models
+{
+    public class PlaylistTrack
+    {
+        public int ChannelId { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Title { get; set; }
+        public string Performer { get; set; }
+        public int? EpisodeId { get; set; }
+    }
+}

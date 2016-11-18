aecf98e90500f5a311082785d64ba91f79851e35
Mattias Rudén <mattias@indiesoft.org>
2016-11-18 10:32:43 +0100

    Modell för program

---
+++ music-taste-based-radio-discovery/music-taste-based-radio-discovery/Models/ProgramModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace music_taste_based_radio_discovery.Models
+{
+    public class ProgramModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Url { get; set; }
+    }
+}
